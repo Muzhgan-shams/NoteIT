c6965ef053d916d70db63af3f08fb140c0f8a9eb
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-02-13 23:29:15 +0500

    chore(deploy): configure production build scripts and static file serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
@@ -8,20 +10,28 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middlewares. order matters
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Vite port
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Vite port
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimit);
 
 // Routes
 app.use("/api/notes", notesRoutes);
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*any", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // app listening to port
 connectDB().then(() => {
   app.listen(PORT, () => {
